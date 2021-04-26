ed370203f24151553807e94db2045ef20924d0ce
Guido-95 <guidocarna1995@gmail.com>
2021-04-26 13:21:12 +0200

    completamento con debug

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+// richiesta nome
+var nome = prompt("Scrivi il tuo nome");
+
+// controllo nome
+console.log(nome);
+
+// richiesta cognome
+var cognome = prompt("Scrivi il tuo cognome");
+
+// controllo cognome
+console.log(cognome);
+
+// richiesta colore
+var colorePreferito = prompt("Scrivi il tuo colore preferito");
+
+// controllo colore
+console.log(colorePreferito);
+
+// inserisci password generata
+var passwordGenerata = document.getElementById("password").innerHTML = (nome + cognome + colorePreferito + "21");
+
+// controllo password generata
+console.log(passwordGenerata);
+
+
